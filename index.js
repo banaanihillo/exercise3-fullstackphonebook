71c8072c604ed8bad3e4c590aeb7efc381c2cc4f
banaanihillo <banaanihillo@protonmail.ch>
2020-04-03 16:43:49 +0300

    Commited use of corse and something something port?

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 var morgan = require("morgan");
+const cors = require('cors')
+app.use(cors())
 app.use(express.json());
+app.use(express.static("build"));
 
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms", {
     skip: function (request, response) {
@@ -89,7 +92,7 @@ app.post("/api/persons", (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Running the server on port ${PORT}`)
 })
