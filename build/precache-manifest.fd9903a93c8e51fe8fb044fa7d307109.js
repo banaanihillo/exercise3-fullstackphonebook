bbffc406af0048a7183c0411f94974d16d78b6b0
banaanihillo <banaanihillo@protonmail.ch>
2020-04-04 11:10:04 +0300

    Added frontend production build to backend

---
+++ build/precache-manifest.fd9903a93c8e51fe8fb044fa7d307109.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b74db9e1bec6bc9845edac1ee23e1836",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92287eba6cbf2d9fa791",
+    "url": "/static/css/main.0417805d.chunk.css"
+  },
+  {
+    "revision": "4e5991ee5799363f1ed9",
+    "url": "/static/js/2.d40cd132.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.d40cd132.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92287eba6cbf2d9fa791",
+    "url": "/static/js/main.97cff0a8.chunk.js"
+  },
+  {
+    "revision": "5a7b66ba50936d0e3553",
+    "url": "/static/js/runtime-main.f0885011.js"
+  }
+]);
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 var morgan = require("morgan");
-const cors = require('cors')
-app.use(cors())
+const cors = require('cors');
+app.use(cors());
 app.use(express.json());
+app.use(express.static("build"));
 
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms", {
     skip: function (request, response) {
